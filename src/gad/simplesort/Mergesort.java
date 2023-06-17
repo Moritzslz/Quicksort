642335d2c8493f0a62b963368b24b26b63a94915
moritzslz <schultzmoritz@gmail.com>
2023-06-17 09:38:06 +0200

    MergeSort

--- src/gad/simplesort/Mergesort.java
+++ src/gad/simplesort/Mergesort.java
@@ -12,11 +12,43 @@ public class Mergesort extends SortAlgorithm {
 
 	@Override
 	public void sort(int[] numbers, Result result, int from, int to) {
-		// TODO
+		if (from >= to) {
+			return;
+		}
+		result.startMergesort(numbers, from, to);
+
+		int mid = (from + to) / 2;
+		sort(numbers, result, from, mid);
+		sort(numbers, result, mid + 1, to);
+		result.logPartialArray(numbers, from, to);
 	}
 
 	public void sort(int[] numbers, Result result, int from, int to, int[] helper) {
-		// TODO
+		int mid = (from + to) / 2;
+		int indexL = from;
+		int indexR = mid + 1;
+		int length = from - to + 1;
+
+		for (int i = 0; i < length; i++) {
+			if (indexL > mid) { // linker Teil leer
+				helper[i] = numbers[indexR];
+				indexR++;
+			} else if (indexR > to) { // rechter Teil leer
+				helper[i] = numbers[indexL];
+				indexL++;
+			} else if (numbers[indexL] <= numbers[indexR]) {
+				helper[i] = numbers[indexL];
+				indexL++;
+			} else {
+				helper[i] = numbers[indexR];
+				indexR++;
+			}
+		}
+
+		// Zurückkopieren
+		for (int i = 0; i < length; i++) {
+			numbers[from + i] = helper[i];
+		}
 	}
 
 	@Override
