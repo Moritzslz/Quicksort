44fd360bec8e0dfba2f25023d2bebcb5f9a9804a
moritzslz <schultzmoritz@gmail.com>
2023-06-18 17:13:38 +0200

    Mergesort Selectionsort

--- src/gad/simplesort/Mergesort.java
+++ src/gad/simplesort/Mergesort.java
@@ -27,12 +27,9 @@ public class Mergesort extends SortAlgorithm {
 			return;
 		}
 
-		// Check is subarray is already in order; if not reset from to first unordered index
-		int index = isSorted(numbers, from, to);
-		if (index == -1) {
+		// Check is array is already in order
+		if (isSorted(numbers, from, to)) {
 			return;
-		} else {
-			from = index;
 		}
 
 		int[] helper = new int[numbers.length];
@@ -44,12 +41,9 @@ public class Mergesort extends SortAlgorithm {
 			return;
 		}
 
-		// Check is subarray is already in order; if not reset from to first unordered index
-		int index = isSorted(numbers, from, to);
-		if (index == -1) {
+		// Check is subarray is already in order
+		if (isSorted(numbers, from, to)) {
 			return;
-		} else {
-			from = index;
 		}
 
 		int mid = (from + to) / 2;
@@ -60,6 +54,10 @@ public class Mergesort extends SortAlgorithm {
 	}
 
 	public void merge(int[] numbers, int left, int mid, int right, int[] helper) {
+		// Check is subarray is already in order
+		if (isSorted(numbers, left, right)) {
+			return;
+		}
 
 		int indexL = left;
 		int indexR = mid + 1;
@@ -87,15 +85,15 @@ public class Mergesort extends SortAlgorithm {
 		}
 	}
 
-	public int isSorted (int[] numbers, int from, int to) {
-		int index = -1;
+	public boolean isSorted (int[] numbers, int from, int to) {
+		boolean isSorted = true;
 		for (int i = from + 1; i <= to; i++) {
 			if (numbers[i-1] >= numbers[i]) {
-				index = i - 1;
+				isSorted = false;
 				break;
 			}
 		}
-		return index;
+		return isSorted;
 	}
 
 	@Override
