49b3b0ea05c0f0c456d51f894a85fc3ede25f354
moritzslz <schultzmoritz@gmail.com>
2023-06-18 18:06:57 +0200

    getMedianPivotFront

--- src/gad/simplesort/DualPivotFinder.java
+++ src/gad/simplesort/DualPivotFinder.java
@@ -11,8 +11,10 @@ public interface DualPivotFinder {
 
 			@Override
 			public int[] findPivot(int[] numbers, int from, int to) {
-				// TODO
-				return null;
+				int[] pivots = new int[2];
+				pivots[0] = from;
+				pivots[1] = to;
+				return pivots;
 			}
 
 			@Override
@@ -28,8 +30,13 @@ public interface DualPivotFinder {
 		return new DualPivotFinder() {
 			@Override
 			public int[] findPivot(int[] numbers, int from, int to) {
-				// TODO
-				return null;
+				int[] pivots = new int[2];
+				pivots[0] = random.nextInt(from, to+1);
+				pivots[1] = random.nextInt(from, to+1);
+				while (pivots[1] == pivots[0]) {
+					pivots[1] = random.nextInt(from, to+1);
+				}
+				return pivots;
 			}
 
 			@Override
@@ -43,8 +50,26 @@ public interface DualPivotFinder {
 		return new DualPivotFinder() {
 			@Override
 			public int[] findPivot(int[] numbers, int from, int to) {
-				// TODO
-				return null;
+				int[] pivots = new int[2];
+				pivots[0] = from;
+				pivots[1] = to;
+				int length;
+
+				// Edge cases
+				if (numberOfConsideredElements > to - from + 1) {length = to - from + 1;}
+				else {length = numberOfConsideredElements;}
+				if (length > numbers.length) {length = numbers.length;}
+				if (from == to || from == to - 1) {return pivots;}
+
+				int gap = length / numberOfConsideredElements;
+				if (from == 0) {
+					pivots[0] = gap;
+					pivots[1] = 2 * gap;
+				} else {
+					pivots[0] = from + gap - 1;
+					pivots[1] = from + 2 * gap - 1;
+				}
+				return pivots;
 			}
 
 			@Override
--- src/gad/simplesort/DualPivotQuicksort.java
+++ src/gad/simplesort/DualPivotQuicksort.java
@@ -14,7 +14,64 @@ public class DualPivotQuicksort extends SortAlgorithm {
 
 	@Override
 	public void sort(int[] numbers, Result result, int from, int to) {
-		// TODO
+		if (from >= to) {
+			return;
+		}
+		result.startQuicksort(numbers, from, to);
+
+		// SelectionSort Optimierung
+		if (to - from + 1 <= selectionSortSize) {
+			selectionSort.sort(numbers, result, from, to);
+			return;
+		}
+
+		int mid = numbers.length / 2;
+
+		int[] selectedPivot = pivotFinder.findPivot(numbers, from, to);
+		// Switching the higher Pivot to the last index
+		swap(numbers, selectedPivot[1], to);
+		// Switching the lower Pivot to the first index
+		swap(numbers, selectedPivot[0], from);
+
+		int p1 = numbers[to];
+		int indexL = from - 1;
+		int indexR = mid;
+		while (indexL < indexR) {
+			do {
+				indexL++;
+			} while (numbers[indexL] < p1);
+			do {
+				indexR--;
+			} while (numbers[indexR] > p1 && indexR > from);
+			if (indexL < indexR) {
+				swap(numbers, indexL, indexR);
+			}
+		}
+		// Pivot an richtige Stelle verschieben
+		swap(numbers, indexL, to);
+
+		int p2 = numbers[from];
+		int indexL2 = mid + 1;
+		int indexR2 = to;
+		while (indexL2 < indexR2) {
+			do {
+				indexL2++;
+			} while (numbers[indexL2] < p2);
+			do {
+				indexR--;
+			} while (numbers[indexR2] > p2 && indexR2 > from);
+			if (indexL2 < indexR2) {
+				swap(numbers, indexL2, indexR2);
+			}
+		}
+		// Pivot an richtige Stelle verschieben
+		swap(numbers, indexL2, from);
+
+		// Rekursiv beide Teile weiter sortieren
+		result.logPartialArray(numbers, from, indexL - 1);
+		result.logPartialArray(numbers, indexL + 1, to);
+		sort(numbers, result, from, indexL - 1);
+		sort(numbers, result, indexL + 1, to);
 	}
 
 	@Override
