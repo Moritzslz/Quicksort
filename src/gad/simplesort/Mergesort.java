2a22a4688efdeaf82e38a3233706202a1cc78c56
moritzslz <schultzmoritz@gmail.com>
2023-06-18 17:17:12 +0200

    Selectionsort

--- src/gad/simplesort/Mergesort.java
+++ src/gad/simplesort/Mergesort.java
@@ -27,6 +27,7 @@ public class Mergesort extends SortAlgorithm {
 
 		// SelectionSort Optimierung
 		if (to - from + 1 <= selectionSortSize) {
+			result.logPartialArray(numbers, from, to);
 			selectionSort.sort(numbers, result, from, to);
 			return;
 		}
